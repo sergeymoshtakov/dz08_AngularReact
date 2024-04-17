a66233ca63e518d5c1d591d53f9cf149b30ee83a
Serhii Moshtakov <serhii.moshtakov@students.fhnw.ch>
2024-04-17 17:12:32 +0200

    added validation

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -39,17 +39,41 @@ export class OrderComponent {
       }),
       delivery : new FormGroup({
         todelivery : new FormControl(''),
-        adress : new FormControl('', [Validators.required]),
+        adress : new FormControl('', { validators: [], updateOn: 'change' }),
       }),
       payment : new FormGroup({
         topayment : new FormControl(''),
-        cardnumber : new FormControl('', [Validators.required]),
+        cardnumber : new FormControl('', { validators: [], updateOn: 'change' }),
       }),
     },);
+
+    this.onChanges();
   }
 
   formData = {}
   isSubmited : boolean = false;
+
+  onChanges() {
+    this.form.get('delivery.todelivery')?.valueChanges.subscribe(val => {
+      const addressControl = this.form.get('delivery.adress');
+      if (val === 'delivery') {
+        addressControl?.setValidators([Validators.required]);
+      } else {
+        addressControl?.clearValidators();
+      }
+      addressControl?.updateValueAndValidity();
+    });
+
+    this.form.get('payment.topayment')?.valueChanges.subscribe(val => {
+      const cardNumberControl = this.form.get('payment.cardnumber');
+      if (val === 'card') {
+        cardNumberControl?.setValidators([Validators.required, Validators.pattern('[0-9]{16}')]);
+      } else {
+        cardNumberControl?.clearValidators();
+      }
+      cardNumberControl?.updateValueAndValidity();
+    });
+  }
 
   onSubmit() : void{
     this.isSubmited = true;
